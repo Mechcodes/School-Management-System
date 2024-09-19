87f795d58337da84e8dde956f148d2cd89ec4c38
Mechcodes <rajathansbora@gmail.com>
2024-09-19 14:42:58 +0530

    some config changes has been added

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // const target = 'http://localhost:3000'
-const target = 'https://school-management-system-backend-hlqh.onrender.com'
+const target2 = 'https://school-management-system-backend-hlqh.onrender.com'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:target,
+        target:target2,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
         secure:false,
       },
     },
